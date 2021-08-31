e650a39266443c7891f1b441dd42f3dce7f32045
Tal Endelman <tal.endelman@tufin.com>
2021-08-31 22:57:27 +0300

    asd

--- src/Cinema/main.java
+++ src/Cinema/main.java
@@ -7,21 +7,20 @@ import People.Student;
 import java.util.Arrays;
 
 public class main {
-//    public static void main(String[] args) {
-//    Customer andy = new Customer("tal", 10, "tal", "0123", 12);
-//    Soldier niv = new Soldier("Niv", 11, "tal", "0123", 19, "1.9.2018");
-//    Student omri = new Student("Omri", 12, "tal", "0123", 19, "5.4.20", "1.8.22");
-//    Employee tomer = new Employee("Tomer", 14, "tomer@tomer.com", "2456", 22);
-//    System.out.println("The popcorn price is");
-//    System.out.println(tomer.salePopcorn("small", niv));
-//    System.out.println(tomer.salePopcorn("large", omri));
-//
+    public static void main(String[] args) {
+
+        System.out.println("Welcome To Badli and Endelman Theater! ");
+        Customer andy = new Customer("tal", 10, "tal", "0123", 12);
+        Soldier niv = new Soldier("Niv", 11, "tal", "0123", 19, "1.9.2018");
+        Student omri = new Student("Omri", 12, "tal", "0123", 19, "5.4.20", "1.8.22");
+        Employee tomer = new Employee("Tomer", 14, "tomer@tomer.com", "2456", 22);
+        System.out.println("The popcorn price is");
+        System.out.println(tomer.salePopcorn("small", niv));
+        System.out.println(tomer.salePopcorn("large", omri));
+
 
 
 
 }
-//    andy.equals(niv);
-
-
 }
 
--- src/CinemaPackage/Cinema.java
+++ src/CinemaPackage/Cinema.java
@@ -1,30 +1,69 @@
 package CinemaPackage;
 
+import People.Customer;
+import People.Employee;
+import auditorium.Auditorium;
+
+import java.util.ArrayList;
+
 public class Cinema  {
-    int numberOfCinema;
-    Movie movieDisplayed;
-    boolean[][] seats;
+    String cinemaName;
+    ArrayList<Employee> employeesList;
+    ArrayList<Customer> customerList;
+    Movie[] moviesArray;
+    Auditorium[] auditoriumArray;
 
-    public Cinema(int numberOfCinema, Movie movieDisplayed, boolean[][] seats) {
-        this.numberOfCinema = numberOfCinema;
-        this.movieDisplayed = movieDisplayed;
-        this.seats = seats;
+    public Cinema(String cinemaName, ArrayList<Employee>, int moviesArrayLegnth, int auditoriumArrayLength) {
+        this.cinemaName = cinemaName;
+        this.employeesList = new ArrayList<Employee>;
+        this.customerList = new ArrayList<Customer>;
+        this.moviesArray = new Movie[moviesArrayLegnth];
+        this.auditoriumArray = new Auditorium[auditoriumArrayLength];
     }
 
-    public void setSeats(boolean[][] seats) {
-        this.seats = seats;
+    public void addNewMovie(Movie movieToAdd){
+
+        // Add to check if movie is exist
+
+        int avaiavailableMovieIndex = returnMovieLastIndex();
+
+        if (avaiavailableMovieIndex == -1){
+            System.out.println("Movie array is full! please delete a movie and try to add a movie again, later. EXITING...");
+            return;
+        }
+        // need to add movie to audt
+
     }
 
-    public boolean freeSeat(boolean[][] seats)
-    {
 
-        boolean isFreeSeat = false;
-        int row = seats.length;
-        int column = seats[0].length;
+    public int returnMovieLastIndex(){
+        int index = 0;
+        // If the list is empty return 0 as first index available
+        if (moviesArray[0] == null){
+            return 0;
+        }
 
+        // Search for the first null appearance
+        for (Movie movie : moviesArray) {
+            if (movie == null){
+                return index;
+            }
+            index++;
+        }
 
+        // If the list is full, return non exist index as false:
+        return -1;
 
     }
+
+    public int returnAuditoriumLatestIndex(){
+
+    }
+
+
+
+
+
 
 
 }
